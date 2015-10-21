fd3c02abf395e63077fa2902741d1c30fa822387
AlexB44 <a.bodier130@gmail.com>
2015-10-21 15:09:36 +0200

    Overflow hidden, scroll plus visible

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // DATABASE CONFIG
 var Sequelize = require('sequelize');
 
-module.exports = new Sequelize('tidmincoin', 'root', 'jecode4laloupe', {
+module.exports = new Sequelize('tidmincoin', 'root', 'cuicuishoushou', {
   host: 'localhost',
   dialect: 'mysql'
 });
