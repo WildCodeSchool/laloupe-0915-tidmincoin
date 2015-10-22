acce4ee1bf85e2dfce92a72e585e088708a6dd41
AlexB44 <a.bodier130@gmail.com>
2015-10-22 15:19:34 +0200

    ajout menu deroulant dans recherche

--- public/js/controllers/rechercheController.js
+++ public/js/controllers/rechercheController.js
@@ -1,10 +1,4 @@
-app.directive('myOnKeyDownCall', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {            
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
-                event.preventDefault();
-        });
-    };
-});
+dialecteService.get().then(function(res){
+		$scope.dialectes = res.data;
+	});
+}
